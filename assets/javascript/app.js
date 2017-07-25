4c77e67ddeb6daec1c752c6a6699d5e3d5cb5557
Matthew Drumheller <MattDrummy@gmail.com>
2017-07-25 12:42:32 -0600

    testing new query method

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,46 @@
 $(document).ready(function(){
-  // const url = 'https://nameless-woodland-75573.herokuapp.com/api/v1/games'
-  const url = 'https://localhost:8080/api/v1/games'
+  const url = 'https://nameless-woodland-75573.herokuapp.com/api/v1/games'
+  // const url = 'https://localhost:8080/api/v1/games'
 
   $('.btn').click(function(event){
-    console.log(this.name);
     $('.pageDisplay').css('display', 'none');
     $(`#${this.name}`).css('display', 'block');
   })
 
-// $.get(url)
-//   .then(readDatabase)
-
-$.get(`${url}/1`)
-  .then(readDatabase)
+  $('#readSel').change(readSingleGame)
+  $.get(url).then(readSelectBox)
+  $.get(url).then(readDatabase)
 
 });
 
 function readDatabase(data) {
-  console.log(data);
-  var source   = $("#database").html();
-  var template = Handlebars.compile(source);
-  var context = {
+  $('#read').empty();
+  let source = $("#database").html();
+  let template = Handlebars.compile(source);
+  let context = {
     games: data,
     name: data.name,
     developer: data.developer,
     directors: data.directors,
     year: data.year
   };
-  var html    = template(context);
-  $('#read').append(html)
+  let html = template(context);
+  $('#read').append(html);
+}
+
+function readSingleGame(event) {
+  let name = $('#readSel').text();
+  $.get(`${url}/${name}`)
+    .then(readDatabase)
+}
+
+function readSelectBox(data) {
+  let source = $("#readSelectBox").html();
+  let template = Handlebars.compile(source);
+  let context = {
+    numbers: data,
+    name: data.name
+  };
+  let html = template(context);
+  $('#readSel').append(html);
 }
