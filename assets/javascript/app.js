19779be294146ea37e29a460cf6c30e904c713d3
Matthew Drumheller <MattDrummy@gmail.com>
2017-07-25 11:58:26 -0600

    created page framework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,32 @@
-const url = 'http://localhost:8080/api/v1/games'
-$.get(url)
-  .then((data) =>{
-    console.log(data);
-    $('body').append(`
-      ${JSON.stringify(data)}
-      `)
+$(document).ready(function(){
+  // const url = 'https://nameless-woodland-75573.herokuapp.com/api/v1/games'
+  const url = 'https://localhost:8080/api/v1/games'
+
+  $('.btn').click(function(event){
+    console.log(this.name);
+    $('.pageDisplay').css('display', 'none');
+    $(`#${this.name}`).css('display', 'block');
   })
 
-  $.get(`${url}/1`)
-    .then((data) => {
-      console.log(data);
-      $('body').append(`
-        ${JSON.stringify(data)}
-        `)
-    })
+// $.get(url)
+//   .then(readDatabase)
+
+$.get(`${url}/1`)
+  .then(readDatabase)
+
+});
+
+function readDatabase(data) {
+  console.log(data);
+  var source   = $("#database").html();
+  var template = Handlebars.compile(source);
+  var context = {
+    games: data,
+    name: data.name,
+    developer: data.developer,
+    directors: data.directors,
+    year: data.year
+  };
+  var html    = template(context);
+  $('#read').append(html)
+}
